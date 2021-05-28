aa2901260cddb18e89e8b548570c150421494467
hideru <jota.16.stigma.world@gmail.com>
2021-05-28 18:22:32 +0900

    added save function, but it failed

--- CharaEditor/MainWindow.xaml.cs
+++ CharaEditor/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,36 @@ namespace CharaEditor
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void CreateSaveBitmap(Canvas canvas, string src)
+        {
+            RenderTargetBitmap bmp = new RenderTargetBitmap((int) canvas.Width, (int) canvas.Height, 96d, 96d,
+                PixelFormats.Pbgra32);
+            canvas.Measure(new Size((int)canvas.Width, (int)canvas.Height));
+            canvas.Arrange(new Rect(new Size((int)canvas.Width, (int)canvas.Height)));
+            
+            bmp.Render(canvas);
+
+            PngBitmapEncoder encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(bmp));
+
+            using (FileStream file = File.Create(src))
+            {
+                encoder.Save(file);
+            }
+        }
+        
+        private void SaveUnifiedImage(object sender, RoutedEventArgs e)
+        {
+            // throw new System.NotImplementedException();
+            CreateSaveBitmap(canvas1, @"C:\Users\Enin\OneDrive\test\out.png");
+            // string output = @"C:\Users\Enin\OneDrive\test\out.png";
+            // if (File.Exists(output))
+            // {
+            //     output = @"C:\Users\Enin\OneDrive\test\out2.png";
+            // }
+            // CreateSaveBitmap(canvas1, output);
         }
     }
 }
